30076ed7a91339d9b35c811e1ce361dc99f02f9d
Adam Zolnai <adam.zolnai@icloud.com>
2025-03-18 09:44:30 -0400

    Acquisition class done

--- __main__.py
+++ __main__.py
@@ -1,10 +1,18 @@
 from Acquisition import Acquisition
 
+"""
+Aquire voltage across gold wire and source channel.
+
+Arguments:
+    - gold_channel: the voltage across the gold wire
+    - source_channel: the voltage across the whole circuit (so the output of the source)
+"""
+
 acquisition = Acquisition(
-    gold_channel = "tempSensor1/ai0",  # Channel lié à la résistance connue
-    source_channel = "tempSensor1/ai1",  # Channel lié à la résistance inconnue
-    # number_of_samples_per_channel = 2,  # nombre de points pris par mesures
-    samples_by_second = 10000,
+    gold_channel = "tempSensor1/ai0",
+    source_channel = "tempSensor1/ai1",
+    # samples_by_second = 10000,
+    # number_of_samples_per_channel = 2,
     # export_target = r"acquisition_data.csv"
 )
 
