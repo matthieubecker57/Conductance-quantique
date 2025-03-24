4643eac2f4c696b6b579866f8e5c4b654e142d02
Adam Zolnai <adam.zolnai@icloud.com>
2025-03-24 10:01:08 -0400

    new data set

--- __main__.py
+++ __main__.py
@@ -13,7 +13,7 @@ Range width corresponds to the analog input range used. The NimyDAQ has either +
     corresponding with a width of 4 or 20 V, respectively.
 Number of bits is given by the manifacturer
 """
-Range_width = 4,
+Range_width = 4
 Number_of_bits = 16
 
 
@@ -32,16 +32,16 @@ Arguments:
     - source_channel: the voltage across the whole circuit (so the output of the source)
 """
 
-acquisition = Acquisition(
-    gold_channel = "DAQ_team_3_PHS2903/ai1",
-    source_channel = "DAQ_team_3_PHS2903/ai0",
-    samples_by_second = 100000,
-    # number_of_samples_per_channel = 2,
-    # export_target = r"acquisition_data.csv"
-)
+# acquisition = Acquisition(
+#     gold_channel = "DAQ_team_3_PHS2903/ai0",  # Across the gold wires
+#     source_channel = "DAQ_team_3_PHS2903/ai1",  # Across the source
+#     samples_by_second = 100000,
+#     # number_of_samples_per_channel = 2,
+#     # export_target = r"acquisition_data.csv"
+# )
 
-acquisition.continuous_acquisition()
-acquisition.export_to_csv()
+# acquisition.continuous_acquisition()
+# acquisition.export_to_csv()
 
 """
 ----------------------------------------------------------------------------------------------
