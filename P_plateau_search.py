a3ff548ceb0d97194ae4a61eb29caff2ffb41917
Adam Zolnai <adam.zolnai@icloud.com>
2025-03-27 16:11:29 -0400

    added description to code

--- P_plateau_search.py
+++ P_plateau_search.py
@@ -4,6 +4,13 @@ import matplotlib.pyplot as plt
 from MathCore import compute_conductance_order, compute_conductance
 
 """
+This file will search trough the filtered data in P_filtered_data.csv and search for a series of point 
+(V_{i}, V_{i+1},...,V_{i+n}) that satisfies the following condition:
+The difference |V_{m+1} - V_{m}| between any two points in the series is smaller than a certain threshold.
+It then plot the plateaus found, as well as multiple other variants (conductance, averages, etc.)
+"""
+
+"""
 Define the data to search in
 """
 
@@ -54,12 +61,12 @@ plt.plot(
     Vwire.index.to_list(),
     Vwire,
     'o',
-    markersize=1,
+    markersize=2.5,
     color='black')
 
 for index in plateaus.keys():
     indexes = plateaus[index].index.tolist()
-    plt.plot(indexes, [Vwire[index] for index in indexes], 'o', markersize=2),
+    plt.plot(indexes, [Vwire[index] for index in indexes], 'o', markersize=3.5),
 
 plt.title("Les différents plateaux identifiés")
 plt.ylabel("Tension (V)")
@@ -71,18 +78,18 @@ plt.show()
 Plots the average voltage found for each plateau
 """
 
-# plateau_values = [np.mean(plateaus[key]) for key in plateaus.keys()]
-# plt.plot(
-#     plateaus.keys(),
-#     plateau_values,
-#     'o',
-#     markersize=2
-# )
-# plt.title("Les valeurs moyennes des divers plateaus identifiés")
-# plt.ylabel("Tension (V)")
-# plt.xlabel("Temps auquel le premier point sur le plateau a été identifié (10 microsecondes)")
-# plt.grid(which='both')
-# plt.show()
+plateau_values = [np.mean(plateaus[key]) for key in plateaus.keys()]
+plt.plot(
+    plateaus.keys(),
+    plateau_values,
+    'o',
+    markersize=2
+)
+plt.title("Les valeurs moyennes des divers plateaus identifiés")
+plt.ylabel("Tension (V)")
+plt.xlabel("Temps auquel le premier point sur le plateau a été identifié (10 microsecondes)")
+plt.grid(which='both')
+plt.show()
 
 """
 Plot the plateaus as conductance
